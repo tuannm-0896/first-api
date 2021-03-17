6087d2f6753a884eaca48b5c084fe293e96b5b13
TuanNM <nguyen.minh.tuan@sun-asterisk.com>
2021-03-17 22:29:00 +0700

    Refactor 1

--- Models/UserModel.go
+++ Models/UserModel.go
@@ -1,11 +1,13 @@
 package Models
+
 type User struct {
- Id      uint   `json:"id"`
- Name    string `json:"name"`
- Email   string `json:"email"`
- Phone   string `json:"phone"`
- Address string `json:"address"`
+	Id      uint   `json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Phone   string `json:"phone"`
+	Address string `json:"address"`
 }
+
 func (b *User) TableName() string {
- return "user"
+	return "user"
 }
